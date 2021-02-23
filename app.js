cbdfc5c8f3fce0b627a00e23c735cdfa3cb744f5
bae-seongkyu <75983289+pangkyu@users.noreply.github.com>
2021-02-23 21:26:19 +0900

    Update app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fill
+let painting = false;
+
+function stopPainting(){
+    painting = false;
+}
+
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY;    
+}
+
+function onMouseDown(event){
+    painting = true;
+}
+
+function onMouseUp(event){
+    stopPainting();
+}
+
+if(canvas){
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseleave", stopPainting);
+}
